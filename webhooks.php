0eae03a0b91a3af19210b9e29d6cc4bdb0eb90fd
lockonzoom <47096743+lockonzoom@users.noreply.github.com>
2019-01-28 11:41:17 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'U7jBtvsMjfHYhH6+tzUaNBbo6DwoSplWMGOjoerGIblQ3f7CiirlkK25AOmLg8+lk1X1d9CGtyPJ5iZLbudv/G5xN9CK/3TdPIlQmza6DXoGBUBi/Viogfnx+jeJg5E0v+fyTHTppU+KPio/BQjtKwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
